312530059631edbdb56154e305399f5770c9e8de
eetan2000 <ee_tan@cs.sfu.ca>
2022-07-26 15:17:46 -0700

    fixed

--- SpaceQL-INSERT.sql
+++ SpaceQL-INSERT.sql
@@ -1,17 +1,17 @@
 INSERT INTO SpaceAgencies VALUES ('1','USA','1958-07-29','NASA','Bill Nelson');
 INSERT INTO SpaceAgencies VALUES ('2','USA','2002-03-14','SpaceX','Elon Musk');
 
-INSERT INTO DiscoveredLargeObjects VALUES ('0000', 'Earth', '12742', '5.972 * 10^24', NULL);
-INSERT INTO DiscoveredLargeObjects VALUES ('0001', 'Sun', '1392700', '1.989 * 10^30', NULL);
-INSERT INTO DiscoveredLargeObjects VALUES ('0002', 'Mercury', '4829', '3.285 * 10^23', NULL);
-INSERT INTO DiscoveredLargeObjects VALUES ('0003', 'Venus', '12104', '4.867 * 10^24', NULL );
-INSERT INTO DiscoveredLargeObjects VALUES ('0004', 'Mars', '6779', '6.39 * 10^23', NULL);
-INSERT INTO DiscoveredLargeObjects VALUES ('0005', 'Jupiter', '139820', '1.898 * 10^27', NULL);
-INSERT INTO DiscoveredLargeObjects VALUES ('0006', 'Saturn', '116460', '5.683 * 10^26', NULL);
-INSERT INTO DiscoveredLargeObjects VALUES ('0007', 'Neptune', '49244', '1.024 * 10^26', NULL);
-INSERT INTO DiscoveredLargeObjects VALUES ('0008', 'Pluto', '2376', '1.309 * 10^22', NULL);
-INSERT INTO DiscoveredLargeObjects VALUES ('0009', 'Sirius', '2400000', '4.103 * 10^30', NULL);
-INSERT INTO DiscoveredLargeObjects VALUES ('0010', '14 Andromedae b', '160793', '9.110 * 10^27', '1');
+INSERT INTO DiscoveredLargeObjects VALUES ('0000', 'Earth', '12742', '5.972 * 10^24', NULL, NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0001', 'Sun', '1392700', '1.989 * 10^30', NULL, NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0002', 'Mercury', '4829', '3.285 * 10^23', NULL, NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0003', 'Venus', '12104', '4.867 * 10^24', NULL, NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0004', 'Mars', '6779', '6.39 * 10^23', NULL, NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0005', 'Jupiter', '139820', '1.898 * 10^27', NULL, NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0006', 'Saturn', '116460', '5.683 * 10^26', NULL, NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0007', 'Neptune', '49244', '1.024 * 10^26', NULL, NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0008', 'Pluto', '2376', '1.309 * 10^22', NULL, NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0009', 'Sirius', '2400000', '4.103 * 10^30', NULL, NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0010', '14 Andromedae b', '160793', '9.110 * 10^27', '1', '2008-07-02');
 
 
 INSERT INTO Planets VALUES ('0000', '365', '24');
