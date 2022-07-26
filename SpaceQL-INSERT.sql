8e7cff350960f9104faec0b93b6ace352209aa05
eetan2000 <ee_tan@cs.sfu.ca>
2022-07-26 15:06:57 -0700

    added galaxies space agency and solar system

--- SpaceQL-INSERT.sql
+++ SpaceQL-INSERT.sql
@@ -1,3 +1,6 @@
+INSERT INTO SpaceAgencies VALUES ('1','USA','1958-07-29','NASA','Bill Nelson');
+INSERT INTO SpaceAgencies VALUES ('2','USA','2002-03-14','SpaceX','Elon Musk');
+
 INSERT INTO DiscoveredLargeObjects VALUES ('0000', 'Earth', '12742', '5.972 * 10^24', NULL);
 INSERT INTO DiscoveredLargeObjects VALUES ('0001', 'Sun', '1392700', '1.989 * 10^30', NULL);
 INSERT INTO DiscoveredLargeObjects VALUES ('0002', 'Mercury', '4829', '3.285 * 10^23', NULL);
@@ -8,7 +11,7 @@ INSERT INTO DiscoveredLargeObjects VALUES ('0006', 'Saturn', '116460', '5.683 * 
 INSERT INTO DiscoveredLargeObjects VALUES ('0007', 'Neptune', '49244', '1.024 * 10^26', NULL);
 INSERT INTO DiscoveredLargeObjects VALUES ('0008', 'Pluto', '2376', '1.309 * 10^22', NULL);
 INSERT INTO DiscoveredLargeObjects VALUES ('0009', 'Sirius', '2400000', '4.103 * 10^30', NULL);
-
+INSERT INTO DiscoveredLargeObjects VALUES ('0010', '14 Andromedae b', '160793', '9.110 * 10^27', '1');
 
 
 INSERT INTO Planets VALUES ('0000', '365', '24');
@@ -19,6 +22,14 @@ INSERT INTO Planets VALUES ('0005', '4380', '10');
 INSERT INTO Planets VALUES ('0006', '10585', '10');
 INSERT INTO Planets VALUES ('0007', '60225', '16');
 INSERT INTO Planets VALUES ('0008', '90520', '153');
+INSERT INTO Planets VALUES ('0010', '186', '4');
 
 INSERT INTO Stars VALUES ('0001', 'Hydrogen, Helium');
 INSERT INTO Stars VALUES ('0009', 'Carbon, Oxygen');
+
+INSERT INTO Galaxies VALUES ('001','Milky Way', '105,700');
+INSERT INTO Galaxies VALUES ('002', 'Andromeda', '220,000');
+
+INSERT INTO SolarSystemInGalaxy VALUES ('01','001','Our System', '287460000000');
+INSERT INTO SolarSystemInGalaxy VALUES ('02', '001', 'Proxima System', '324460000000');
+
