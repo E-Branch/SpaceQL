9ead01db0ebea910649f8fbe3fef9426fb765e0d
eetan2000 <ee_tan@cs.sfu.ca>
2022-07-26 14:28:15 -0700

    INSERT script

---
+++ SpaceQL-INSERT.sql
@@ -0,0 +1,24 @@
+INSERT INTO DiscoveredLargeObjects VALUES ('0000', 'Earth', '12742', '5.972 * 10^24', NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0001', 'Sun', '1392700', '1.989 * 10^30', NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0002', 'Mercury', '4829', '3.285 * 10^23', NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0003', 'Venus', '12104', '4.867 * 10^24', NULL );
+INSERT INTO DiscoveredLargeObjects VALUES ('0004', 'Mars', '6779', '6.39 * 10^23', NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0005', 'Jupiter', '139820', '1.898 * 10^27', NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0006', 'Saturn', '116460', '5.683 * 10^26', NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0007', 'Neptune', '49244', '1.024 * 10^26', NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0008', 'Pluto', '2376', '1.309 * 10^22', NULL);
+INSERT INTO DiscoveredLargeObjects VALUES ('0009', 'Sirius', '2400000', '4.103 * 10^30', NULL);
+
+
+
+INSERT INTO Planets VALUES ('0000', '365', '24');
+INSERT INTO Planets VALUES ('0002', '88', '1407');
+INSERT INTO Planets VALUES ('0003', '225', '2802');
+INSERT INTO Planets VALUES ('0004', '687', '24');
+INSERT INTO Planets VALUES ('0005', '4380', '10');
+INSERT INTO Planets VALUES ('0006', '10585', '10');
+INSERT INTO Planets VALUES ('0007', '60225', '16');
+INSERT INTO Planets VALUES ('0008', '90520', '153');
+
+INSERT INTO Stars VALUES ('0001', 'Hydrogen, Helium');
+INSERT INTO Stars VALUES ('0009', 'Carbon, Oxygen');
